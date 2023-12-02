25d5fbe5f2ae4873d8fc14156e8fc790898502fa
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2023-12-01 17:41:55 -0800

    Creating the sun

--- Solar System Barycenter/ssb.py
+++ Solar System Barycenter/ssb.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 spiceypy.furnsh("../Solar System Barycenter/kernel_meta.txt")
 spiceypy.furnsh("../Kernels/pck/pck00010.tpc.txt")
 spiceypy.furnsh("../Kernels/lsk/naif0012.tls.txt")
@@ -25,8 +24,10 @@ ssb_wrt_sun_position = []
 time_interval_et = np.linspace(init_time_et, end_time_et, delta_days)
 
 for time_interval_et_f in time_interval_et:
-    _position, _ = spiceypy.spkgps(targ=0, et=time_interval_et_f, ref="ECLIPJ2000", obs=10)
-    
+    _position, _ = spiceypy.spkgps(
+        targ=0, et=time_interval_et_f, ref="ECLIPJ2000", obs=10
+    )
+
     ssb_wrt_sun_position.append(_position)
 
 ssb_wrt_sun_position = np.array(ssb_wrt_sun_position)
@@ -38,4 +39,15 @@ ssb_wrt_sun_position_scaled = ssb_wrt_sun_position / radii_sun
 
 ssb_wrt_sun_position_scaled_xy = ssb_wrt_sun_position_scaled[:, 0:2]
 plt.style.use("dark_background")
-fig, ax = plt.subplots(figsize=(12, 8))
+fig, ax = plt.subplots(figsize=(12, 12))
+
+sun_circ = plt.Circle((0.0, 0.0, 0.0), 1.0, color="yellow", alpha=0.6)
+ax.add_artist(sun_circ)
+
+ax.set_aspect("equal")
+ax.grid(True, linestyle="dashed", alpha=0.5)
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+
+ax.set_xlabel("x in sun-radii")
+ax.set_ylabel("y in sun-radii")
