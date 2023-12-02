7842d4840dd2bc15b7c3b6e51a851c7ea4d036ec
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2023-12-01 17:36:49 -0800

    Sun radii

--- Solar System Barycenter/ssb.py
+++ Solar System Barycenter/ssb.py
@@ -1,6 +1,9 @@
 import datetime
 import spiceypy
 import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 spiceypy.furnsh("../Solar System Barycenter/kernel_meta.txt")
 spiceypy.furnsh("../Kernels/pck/pck00010.tpc.txt")
@@ -29,3 +32,10 @@ for time_interval_et_f in time_interval_et:
 ssb_wrt_sun_position = np.array(ssb_wrt_sun_position)
 
 _, radii_sun = spiceypy.bodvcd(bodyid=10, item="RADII", maxn=3)
+
+radius_sun = radii_sun[0]
+ssb_wrt_sun_position_scaled = ssb_wrt_sun_position / radii_sun
+
+ssb_wrt_sun_position_scaled_xy = ssb_wrt_sun_position_scaled[:, 0:2]
+plt.style.use("dark_background")
+fig, ax = plt.subplots(figsize=(12, 8))
