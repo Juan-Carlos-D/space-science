458b924a49664265626f02afc444e03acf5eb1ee
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2023-12-06 17:41:53 -0800

    solsys df

--- Ecliptic Coordinates/ecliptic_coordinates.py
+++ Ecliptic Coordinates/ecliptic_coordinates.py
@@ -15,3 +15,9 @@ datetime_utc = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
 # Convert to Ephemeris Time (ET) using the SPICE function utc2et
 datetime_et = spiceypy.utc2et(datetime_utc)
+
+solsys_df = pd.DataFrame()
+solsys_df.loc[:, "ET"] = [datetime_et]
+solsys_df.loc[:, "UTC"] = [datetime_utc]
+
+SOLSYS_DICT = {"SUN": 10, "VENUS": 299, "MOON": 301, "MARS": 4}
