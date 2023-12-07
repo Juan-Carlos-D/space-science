3c8f7a12755321576702073e11bf37da3ee9b99e
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2023-12-06 18:22:46 -0800

    ecliptic coordinates

--- Ecliptic Coordinates/ecliptic_coordinates.py
+++ Ecliptic Coordinates/ecliptic_coordinates.py
@@ -46,7 +46,7 @@ for body_name in SOLSYS_DICT:
             targ=SOLSYS_DICT[body_name], et=x, ref="ECLIPJ2000", abcorr="LT+S", obs=399
         )[0]
     )
-    
+
     # Compute the longitude and latitude of the body in radians in ECLIPJ2000
     # using the function recrad. recrad returns the distance, longitude and
     # latitude value; thus, apply [1] and [2] to get the longitude and
@@ -65,7 +65,7 @@ for body_name in SOLSYS_DICT:
 # projected data
 
 # Use a dark background
-plt.style.use('dark_background')
+plt.style.use("dark_background")
 
 # Set a figure
 plt.figure(figsize=(12, 8))
@@ -75,8 +75,88 @@ plt.subplot(projection="aitoff")
 plt.grid(True)
 
 # Set long. / lat. labels
-plt.xlabel('Long. in deg')
-plt.ylabel('Lat. in deg')
+plt.xlabel("Long. in deg")
+plt.ylabel("Lat. in deg")
 
 # Save the figure
-plt.savefig('empty_aitoff.png', dpi=300)
+plt.savefig("empty_aitoff.png", dpi=300)
+
+# Before we plot the data, we need to convert the longitude data into a
+# matplotlib compatible format. We computed longitude values between 0 and
+# 2*pi (360 degrees). matplotlib expects values between -pi and +pi. Further,
+# sky maps count from 0 degrees longitude to the left. Thus we need also to
+# invert the longitude values
+for body_name in SOLSYS_DICT:
+    solsys_df.loc[:, f"{body_name}_long_rad4plot_ecl"] = solsys_df[
+        f"{body_name}_long_rad_ecl"
+    ].apply(lambda x: -1 * ((x % np.pi) - np.pi) if x > np.pi else -1 * x)
+
+# Create now a sky map of the results
+
+# Set a dark background (the night sky is ... dark)
+plt.style.use("dark_background")
+
+# Create a figure and then apply the aitoff projection
+plt.figure(figsize=(12, 8))
+plt.subplot(projection="aitoff")
+
+# Set the UTC time string as a title
+plt.title(f"{datetime_utc} UTC", fontsize=10)
+
+# Each body shall have an individual color; set a list with some colors
+BODY_COLOR_ARRAY = [
+    "y",
+    "tab:brown",
+    "tab:orange",
+    "g",
+    "tab:gray",
+    "tab:red",
+    "m",
+    "tab:olive",
+    "c",
+    "b",
+    "tab:purple",
+]
+
+# Iterate through the pandas dataframe. And plot each celestial body
+for body_name, body_color in zip(SOLSYS_DICT, BODY_COLOR_ARRAY):
+    # Plot the longitude and latitude data. Apply the color, and other
+    # formatting parameters
+    plt.plot(
+        solsys_df[f"{body_name}_long_rad4plot_ecl"],
+        solsys_df[f"{body_name}_lat_rad_ecl"],
+        color=body_color,
+        marker="o",
+        linestyle="None",
+        markersize=12,
+        label=body_name.capitalize(),
+    )
+
+# Replace the standard x ticks (longitude) with the ecliptic coordinates
+plt.xticks(
+    ticks=np.radians([-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150]),
+    labels=[
+        "150°",
+        "120°",
+        "90°",
+        "60°",
+        "30°",
+        "0°",
+        "330°",
+        "300°",
+        "270°",
+        "240°",
+        "210°",
+    ],
+)
+
+# Set the axes labels
+plt.xlabel("Eclip. long. in deg")
+plt.ylabel("Eclip. lat. in deg")
+
+# Create a legend and grid
+plt.legend()
+plt.grid(True)
+
+# Save the figure
+plt.savefig("eclipj2000_sky_map.png", dpi=300)
