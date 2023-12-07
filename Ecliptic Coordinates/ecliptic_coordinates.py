f6683c8b6d88c2cefd240f959cfc48fa403ef6b7
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2023-12-06 17:57:54 -0800

    long and lat coordinates

--- Ecliptic Coordinates/ecliptic_coordinates.py
+++ Ecliptic Coordinates/ecliptic_coordinates.py
@@ -11,7 +11,7 @@ spiceypy.furnsh("../Kernels/lsk/naif0012.tls.txt")
 spiceypy.furnsh("../Kernels/spk/de432s.bsp")
 
 # Create an initial date-time object that is converted to a string
-datetime_utc = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+datetime_utc = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
 # Convert to Ephemeris Time (ET) using the SPICE function utc2et
 datetime_et = spiceypy.utc2et(datetime_utc)
@@ -20,4 +20,43 @@ solsys_df = pd.DataFrame()
 solsys_df.loc[:, "ET"] = [datetime_et]
 solsys_df.loc[:, "UTC"] = [datetime_utc]
 
-SOLSYS_DICT = {"SUN": 10, "VENUS": 299, "MOON": 301, "MARS": 4}
+SOLSYS_DICT = {
+    "SUN": 10,
+    "MERCURY": 1,
+    "VENUS": 299,
+    "EARTH": 3,
+    "MOON": 301,
+    "MARS": 4,
+    "JUPITER": 5,
+    "SATURN": 6,
+    "URANUS": 7,
+    "NEPTUNE": 8,
+    "PLUTO": 9,
+}
+
+
+# Iterate through the dictionary and compute miscellaneous positional
+# parameters
+for body_name in SOLSYS_DICT:
+    # First, compute the directional vector Earth - body in ECLIPJ2000. Use
+    # LT+S light time correction. spkezp returns the directional vector and
+    # light time. Apply [0] to get only the vector
+    solsys_df.loc[:, f"dir_{body_name}_wrt_earth_ecl"] = solsys_df["ET"].apply(
+        lambda x: spiceypy.spkezp(
+            targ=SOLSYS_DICT[body_name], et=x, ref="ECLIPJ2000", abcorr="LT+S", obs=399
+        )[0]
+    )
+
+    solsys_df.loc[:, f"{body_name}_long_rad_ecl"] = solsys_df[
+        f"dir_{body_name}_wrt_earth_ecl"
+    ].apply(
+        lambda x: spiceypy.recrad(x)[1]
+    )
+
+    solsys_df.loc[:, f"{body_name}_lat_rad_ecl"] = solsys_df[
+        f"dir_{body_name}_wrt_earth_ecl"
+    ].apply(
+        lambda x: spiceypy.recrad(x)[2]
+    )
+    
+    solsys_df["SUN_long_rad_ecl"]
